33b6775c39e743012b3d5316c6cb5937d043484d
Joseph Olayanju <horla@MacBook-Pro.local>
2025-12-08 22:58:59 +0100

    chore: remove dist folders from git tracking

--- backend/notification-service/dist/app.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { AmqpConnection, Nack } from '@golevelup/nestjs-rabbitmq';
-import * as sharedTypes from '@liftoff/shared-types';
-export declare class AppService {
-    private readonly amqpConnection;
-    constructor(amqpConnection: AmqpConnection);
-    handleTenantBillingActive(payload: sharedTypes.TenantBillingActivePayload): Promise<Nack | undefined>;
-}
